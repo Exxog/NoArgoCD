c7258da37a24aee7180b8a42fa449a7799887fb8
Exxog <77926404+Exxog@users.noreply.github.com>
2025-03-19 20:53:07 +0000

    updated

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type GitRepo struct{}
+
+type HelmCart struct{
+	repoUrl string
+	path string
+	targetRevision string
+	chart string
+	name string
+}
+
+
+
+func main(){
+	fmt. Println("hello")
+}
+
